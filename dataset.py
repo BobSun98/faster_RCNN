5e1d363e40c269e3b5c9feb095232611f56dc277
bobsun <1029745844@qq.com>
2022-11-14 14:52:29 +0800

    提交信息

---
+++ dataset.py
@@ -0,0 +1,9 @@
+from torch.utils.data import Dataset
+
+class VOCdataset(Dataset):
+    def init(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self, item):
+        pass
