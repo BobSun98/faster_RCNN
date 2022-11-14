d0aefb3be57a5fc57edf990ac19e75c0f469f07b
bobsun <1029745844@qq.com>
2022-11-14 17:04:28 +0800

    提交信息

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from mobilenetv2_model import MobileNetV2
 
 def create_model(num_classes):
-    backbone = MobileNetV2(weights_path="./mobilenet_v2.pth").features
-    backbone.out_channels = num_classes
+    backbone1 = MobileNetV2(weights_path="./mobilenet_v2.pth").features
+    backbone1.out_channels = num_classes
