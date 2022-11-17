427e73799ea26ce9bdc6ec7d08bd55aad0b4d8c0
bobsun <1029745844@qq.com>
2022-11-17 16:18:49 +0800

    提交信息

--- train.py
+++ train.py
@@ -8,3 +8,5 @@ def create_model(num_classes):
     backbone1.out_channels = num_classes
     anchor_generator = AnchorsGenerator(size=(32, 3, 128, 256, 512), aspect_ratios=(0.5, 1.0, 2.0))
     roi_pooler = torchvision.ops.MultiScaleRoIAlign(output_size=[7, 7], sampling_ratio=2)
+def train_one_epic():
+    pass
